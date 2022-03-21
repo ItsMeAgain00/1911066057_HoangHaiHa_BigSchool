5f9b8e5c11b90023fc8d80ff1595cc53f9398429
ItsMeAgain00 <hahoanghai00@gmail.com>
2022-03-21 14:47:48 +0700

    Commit 2:47pm 21/3/2022

--- 1911066057_HoangHaiHa_BigSchool/Controllers/CoursesController.cs
+++ 1911066057_HoangHaiHa_BigSchool/Controllers/CoursesController.cs
@@ -3,15 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _1911066057_HoangHaiHa_BigSchool.Models;
+using _1911066057_HoangHaiHa_BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace _1911066057_HoangHaiHa_BigSchool.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place,
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+        [Authorize]
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+
+            return View(viewModel);
         }
+
+        
     }
 }
